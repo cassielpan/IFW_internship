52111772568df52194e2b1cf24df297b1a320ed7
Zixin  Pan <82441989+cassielpan@users.noreply.github.com>
2022-04-25 10:12:13 +0200

    task from Frauke
    The biggest problem was how to scrape data from dynamic website

---
+++ scrape dynamic table data.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr 22 15:24:41 2022
+
+@author: zixin
+"""
+import time
+import requests
+import pandas as pd
+
+url = 'https://csrbox.org/ajaxdata.php'
+htmls = []
+
+for page in range(1, 4):
+    post_data = (('page', page), ('tab','Ongoing'), ('keyword', ''), ('state', ''))
+    time.sleep(1)
+    print("**scraping: page%d" % page)
+    r = requests.post(url, data=post_data)
+    htmls.append(r.text)
+
+htmls[1]
+
+df = pd.read_html(htmls[1])
+print(len(df))
+print(type(df))
+df[0].head(20)
+
+
+df_list = []
+for html in htmls:
+    df = pd.read_html(html)
+    df_list.append(df[0])
+    
+df_all = pd.concat(df_list)
+
+df_all.head(3)    
+df_all.shape    
+
+df_all[["Project Name", "Name of the Company", "Thematic Area", "Project Budget", "Location"]].to_excel("/Users/zixin/Desktop/csr_data_Ongoing.xlsx", index=False)
---
+++ scrape table data.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr 18 15:57:32 2022
+
+@author: zixin
+"""
+
+
+import time
+import requests
+import pandas as pd
+
+
+
+ # elem = driver.find_element_by_xpath("//*[@class='card m-panel card25']")
+ # elem.click()
+
+htmls = []
+url = 'https://csrbox.org/India-list-CSR-projects-India?page={idx}'
+for idx in range(1, 141):
+    time.sleep(1)
+    print("**scraping: page%d" % idx)
+    r = requests.get(url.format(idx=idx))
+    htmls.append(r.text)
+
+htmls[1]
+
+df = pd.read_html(htmls[1])
+print(len(df))
+print(type(df))
+df[0].head(4)
+
+df_list = []
+for html in htmls:
+    df = pd.read_html(html)
+    df_list.append(df[0])
+    
+df_all = pd.concat(df_list)
+
+df_all.head(3)    
+df_all.shape    
+
+df_all[["Project Name", "Name of the Company", "Thematic Area", "Project Budget", "Location"]].to_excel("/Users/zixin/Desktop/csr_data.xlsx", index=False)
